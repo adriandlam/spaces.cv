6576db1e727198557aa2b7d4de282658f0775317
Adrian Lam <adrianlamdev@gmail.com>
2025-08-10 20:19:10 -0700

    fix: change better auth baseUrl

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,14 +1,17 @@
 import {
-	inferAdditionalFields,
-	magicLinkClient,
+  inferAdditionalFields,
+  magicLinkClient,
 } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-	/** The base URL of the server (optional if you're using the same domain) */
-	baseURL: "http://localhost:3000",
-	plugins: [magicLinkClient(), inferAdditionalFields<typeof auth>()],
+  /** The base URL of the server (optional if you're using the same domain) */
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : `https://${process.env.NEXT_PUBLIC_APP_DOMAIN}`,
+  plugins: [magicLinkClient(), inferAdditionalFields<typeof auth>()],
 });
 
 export const { signIn, signUp, signOut, useSession, getSession } = authClient;
